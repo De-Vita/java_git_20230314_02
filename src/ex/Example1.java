1f811a63e6faee86d9ef260b046c619418867acc
eunsu1 <lu0201@naver.com>
2023-03-14 10:49:27 +0900

    gugudan

---
+++ src/ex/Example1.java
@@ -0,0 +1,14 @@
+package ex;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		for(int num = 2; num < 10; num++) {
+			for(int i=1; i<10; i++) {
+				System.out.println(num+" * "+i+" = "+(num*i));
+			}
+		}
+
+	}
+
+}
