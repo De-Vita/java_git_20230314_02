aeb68516cc6cc96227cd004d7ccb05faed5bd1a6
eunsu1 <lu0201@naver.com>
2023-03-14 10:57:17 +0900

    numbertable

--- src/ex/Example1.java
+++ src/ex/Example1.java
@@ -3,11 +3,9 @@ package ex;
 public class Example1 {
 
 	public static void main(String[] args) {
-		for(int num = 2; num < 10; num++) {
-			for(int i=1; i<10; i++) {
-				System.out.println(num+" * "+i+" = "+(num*i));
-			}
-		}
+		
+		Example2 example2 = new Example2();
+		example2.numberTable();
 
 	}
 
